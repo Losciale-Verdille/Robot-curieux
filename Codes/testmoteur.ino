24c299d18f85431a08c9071391931116cbe0089c
Losciale-Verdille <45843664+Losciale-Verdille@users.noreply.github.com>
2019-03-15 21:53:05 +0100

    Update testmoteur.ino

--- Codes/testmoteur.ino
+++ Codes/testmoteur.ino
@@ -1,3 +1,5 @@
+// Vérification du fonctionnement des moteurs
+
 //-- MOTEUR A --
 int ENA=9; //Connecté à Arduino pin 9(sortie PWM)
 int IN1=4; //Connecté à Arduino pin 4 // en arrière
@@ -8,22 +10,21 @@ int IN3=6; //Connecté à Arduino pin 6 // en avant
 int IN4=7; //Connecté à Arduino pin 7 // en arrière
 
 void setup() {
-
-pinMode(ENA,OUTPUT); // Configurer
-pinMode(ENB,OUTPUT); // les broches
-pinMode(IN1,OUTPUT); // comme sortie
-pinMode(IN2,OUTPUT);
-pinMode(IN3,OUTPUT);
-pinMode(IN4,OUTPUT);
-Serial.begin(9600);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   //on fait avancer les moteurs à pleine puissance
+  digitalWrite(IN1,LOW);
   digitalWrite(IN2,HIGH);
-  digitalWrite(IN1,LOW);      
-  digitalWrite(IN4,LOW);
   digitalWrite(IN3,HIGH);
+  digitalWrite(IN4,LOW);
   analogWrite(ENA,255);
   analogWrite(ENB,255);
 }
